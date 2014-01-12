b784b8e11d86e41003d4e533c16b6082ff0a9d5c
Roman Schmerold (BNoiZe) <bnoize@froxlor.org>
2014-01-12 15:26:59 +0100

    First testing version of brew
    Signed-off-by: Roman Schmerold (BNoiZe) <bnoize@froxlor.org>

--- Formula/froxlor.rb
+++ Formula/froxlor.rb
@@ -1,22 +1,42 @@
-require 'formula'
+require "formula"
 
 class Froxlor < Formula
-  init
-  homepage  'http://www.froxlor.org/'
-  url       'http://files.froxlor.org/releases/froxlor-0.9.31.2.tar.gz'
-  sha1      '7ef60b51da9075448efa7af55857b4692645168d'
-  head      'https://github.com/Froxlor/Froxlor.git'
-  version   '0.9.31.2'
-  
-  depends_on 'php55'
-  depends_on 'mysql'
-  
+  homepage "http://www.froxlor.org/"
+  url "http://files.froxlor.org/releases/froxlor-0.9.31.2.tar.gz"
+  sha1 "7ef60b51da9075448efa7af55857b4692645168d"
+  version "0.9.31.2"
+
   # Webservers we will support
-  if build.include?('with-apache2') ^ build.include?('with-nginx') ^ build.include?('with-lighttpd')
-    raise "Cannot specify more than one webserver to depend on."
+  if (build.include?('with-apache') && build.include?('with-nginx'))
+    || (build.include?('with-nginx') && build.include?('with-lighttpd'))
+    || (build.include?('with-apache') && build.include?('with-lighttpd'))
+
+    raise "You cannot use more than one webserver with froxlor."
+    super
+  elsif build.include?('with-apache')
+    depends_on 'apache2'
+  elsif build.include?('with-lighttpd')
+    depends_on 'lighttpd'
+  else
+    depends_on 'nginx'
   end
-  
-  depends_on 'apache2' if build.include?('with-apache2')
-  depends_on 'nginx' if build.include?('with-nginx')
-  depends_on 'lighttpd' if build.include?('with-lighttpd')
+
+  # Install PHP
+  if build.include?('with-apache')
+    depends_on 'php55'
+  else
+    depends_on 'php55' => ['with-mysql', 'with-fpm', 'with-apache']
+  end
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    #system "make", "install" # if this fails, try separate make/make install steps
+  end
 end
